b88ad7e29a6d22020be7bb7dd98608669318df4f
Qut77 <93013931+Qut77@users.noreply.github.com>
2026-08-11 13:51:11 +0500

    Add calc logics

--- src/Controller/CalcController.php
+++ src/Controller/CalcController.php
@@ -4,15 +4,36 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class CalcController extends AbstractController
 {
-    #[Route('/', name: 'app_calc')]
-    public function index(): Response
+    #[Route('/', name: 'app_calc', methods:['GET', 'POST'])]
+    public function index(Request $request): Response
     {
+        $result = null;
+
+        if($request->isMethod('POST')) {
+            $number1 = (float) $request->request->get('number1');
+            $number2 = (float) $request->request->get('number2');
+            $operator = $request->request->get('operation');
+
+            $result = match($operator){
+                "add" => $number1 + $number2,
+                "subtract" => $number1 - $number2,
+                "multiply" => $number1 * $number2,
+                "divide" => $number2 != 0 ? $number1 / $number2 : "Ошибка деления на ноль",
+            };
+
+            $request->getSession()->set('result', $result);
+            return $this->redirectToRoute('app_calc');
+        }
+        $result = $request->getSession()->get('result');
+        $request->getSession()->remove('result');
+
         return $this->render('calc/index.html.twig', [
-            'controller_name' => 'CalcController',
+            'result' => $result
         ]);
     }
 }
